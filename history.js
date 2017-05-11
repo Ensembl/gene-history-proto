61c0664b78d19c83e6bfa1d79ff71ced388fc46d
jyothishnt <joyjoy.nt@gmail.com>
2017-05-11 13:38:25 +0100

    Display results for dropdown

--- history.js
+++ history.js
@@ -1,6 +1,6 @@
 var historyURL = 'https://ens-hsr.github.io/gene-history-proto/data/history.json.js';
 
-var populateSelectBox = function (dd_changes, dd_rel, dataArray) {
+var populateSelectBox = function (ele, dataArray) {
   var changes = {};
   $.each(dataArray, function (k) {
     $.each(dataArray[k].changes, function(ch) {
@@ -8,22 +8,25 @@ var populateSelectBox = function (dd_changes, dd_rel, dataArray) {
     })
   });
 
-  dd_rel.html('');
-  dd_changes.html('');
-  Object.keys(dataArray).forEach(function(rel) {
-    dd_rel.append("<option>Since release " + rel + "</option>");    
-  })
-
-  dd_changes.append("<option> All changes </option>");
+  ele.html('');
+  ele.append("<option> All changes </option>");
   Object.keys(changes).forEach(function(change) {
     var icon = '<span class="glyphicon glyphicon-unchecked aria-hidden="true"></span>';
-    dd_changes.append("<option>" + change + "</option>");
+    var disabled = 'disabled';
+    if (dataArray[$('#dd_rel').val()]['changes']) {
+      disabled = (dataArray[$('#dd_rel').val()]['changes'][change])? '' : 'disabled';
+    }
+    ele.append("<option "+ disabled +">" + change + "</option>");
   });
-
-  $('#dd_changes').selectpicker();
-
 };
 
+var populateReleaseBox = function (ele, dataArray) {
+  ele.html('');
+  ele.html('');
+  Object.keys(dataArray).reverse().forEach(function(rel) {
+    ele.append("<option value="+ rel +">Since release " + rel + "</option>");    
+  });
+};
 
 $(document).on('ready', function() {
   $.ajax({
@@ -31,7 +34,30 @@ $(document).on('ready', function() {
     dataType: 'json',
     success: function (json) {
       addHistorySVG(json, $('._svg_container'));
-      populateSelectBox($('#dd_changes'), $('#dd_rel'), json);
+      populateReleaseBox($('#dd_rel'), json);
+      populateSelectBox($('#dd_changes'), json);
+
+      $('#dd_changes').on('change', function() {
+        var change_key = $(this).val();
+        var rel_key = $('#dd_rel').val();
+
+        if (json[rel_key]['changes'] && json[rel_key]['changes'][change_key]) {
+          // console.log(json[rel_key]['changes'][key]);
+          var html = '<p class="title">'+ change_key +'</p>';
+          $.each(json[rel_key]['changes'][change_key], function(i, val) {
+            html += '<ul class="changes">';
+            html += '<li>'+ val +'</li>';
+            html += '</ul>';
+          })
+          $('.results').html(html);
+        }
+      });
+
+      $('#dd_rel').on('change', function() {
+        populateSelectBox($('#dd_changes'), json);
+      });
+
     }
   });
+
 });
