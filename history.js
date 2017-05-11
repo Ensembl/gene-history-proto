00b9a97a1a8e7a2f712d0dc068756106bed809dd
jyothishnt <joyjoy.nt@gmail.com>
2017-05-11 13:07:23 +0100

    selectpicker

--- history.js
+++ history.js
@@ -1,19 +1,27 @@
 var historyURL = 'https://ens-hsr.github.io/gene-history-proto/data/history.json.js';
 
 var populateSelectBox = function (dd_changes, dd_rel, dataArray) {
-        var changes = {};
-    $.each(dataArray, function (k) {
-        $.each(dataArray[k].changes, function(ch) {
-                changes[ch]++;
-      })
-    });
+  var changes = {};
+  $.each(dataArray, function (k) {
+    $.each(dataArray[k].changes, function(ch) {
+      changes[ch]++;
+    })
+  });
 
-        Object.keys(dataArray).forEach(function(rel) {
-            dd_rel.append("<option>Since release " + rel + "</option>");    
-    })
-        Object.keys(changes).forEach(function(change) {
-            dd_changes.append("<option>" + change + "</option>");    
-    })
+  dd_rel.html('');
+  dd_changes.html('');
+  Object.keys(dataArray).forEach(function(rel) {
+    dd_rel.append("<option>Since release " + rel + "</option>");    
+  })
+
+  dd_changes.append("<option> All changes </option>");
+  Object.keys(changes).forEach(function(change) {
+    var icon = '<span class="glyphicon glyphicon-unchecked aria-hidden="true"></span>';
+    dd_changes.append("<option>" + change + "</option>");
+  });
+
+  $('#dd_changes').selectpicker();
+
 };
 
 
