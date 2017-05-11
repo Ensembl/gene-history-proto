01abc1fefa5a9a19724d4b1d5555ac3e92d40f46
Ridwan Amode <ridwan@ebi.ac.uk>
2017-05-11 11:40:55 +0100

    Update history.json.js

--- data/history.json.js
+++ data/history.json.js
@@ -1,1 +1,56 @@
 
+{
+	"89": {
+		"date": {
+			"month": "6",
+			"year": "2017"
+		},
+		"changes": {
+			"Updated transcript": ["FOXP2-202(ENST00000403559.5)"]
+		}
+	},
+
+	"88": {
+		"date": {
+			"month": "3",
+			"year": "2017"
+		},
+		"changes": {
+			"New transcript": ["HLA-B-201(ENST00000639564.1)","GNAS-072(ENST00000639562.1)"],
+			"Retired transcript": ["ENST0034"]
+		}
+	},
+
+	"83": {
+		"date": {
+			"month": "12",
+			"year": "2015"
+		},
+		"changes": {
+			"New transcript": ["ENST0003"],
+			"Updated transcript": ["BRCA2-201(ENST00000544455.5)"]
+		}
+	},
+
+	"76": {
+		"date": {
+			"month": "8",
+			"year": "2014"
+		},
+		"changes": {
+			"Sequence changed": ["ENST00342", "ENST000231", "ENST1111"],
+			"New transcript": ["PCDHA11-002(ENST00000616325.1)", "MED25-201(ENST00000612791.4)","MED25-203(ENST00000617849.4),PXN-AS1-006(ENST00000620336.1)"],
+			"Assembly change": "GRCH38"
+		}
+	},
+
+	"75": {
+		"date": {
+			"month": "2",
+			"year": "2014"
+		},
+		"changes": {
+			"Retired transcript": ["ENST0032", "ENT00132"]
+		}
+	}
+}
