c1956a53efd4b5eeb2c02643afa586253bc4860a
Ridwan Amode <ridwan@ebi.ac.uk>
2017-05-11 12:47:52 +0100

    Update history.json.js

--- data/history.json.js
+++ data/history.json.js
@@ -1,4 +1,3 @@
-
 {
 	"89": {
 		"date": {
@@ -16,8 +15,36 @@
 			"year": "2017"
 		},
 		"changes": {
-			"New transcript": ["HLA-B-201(ENST00000639564.1)","GNAS-072(ENST00000639562.1)"],
-			"Retired transcript": ["ENST0034"]
+			"New transcript": ["HLA-B-201(ENST00000639564.1)", "GNAS-072(ENST00000639562.1)"],
+			"Retired transcript": ["NDUFA10-004(ENST00000626224.2)"]
+		}
+	},
+
+	"87": {
+		"date": {
+			"month": "12",
+			"year": "2016"
+		}
+	},
+
+	"86": {
+		"date": {
+			"month": "10",
+			"year": "2016"
+		}
+	},
+
+	"85": {
+		"date": {
+			"month": "7",
+			"year": "2016"
+		}
+	},
+
+	"84": {
+		"date": {
+			"month": "3",
+			"year": "2016"
 		}
 	},
 
@@ -27,8 +54,49 @@
 			"year": "2015"
 		},
 		"changes": {
-			"New transcript": ["ENST0003"],
-			"Updated transcript": ["BRCA2-201(ENST00000544455.5)"]
+			"Updated transcript": ["TXNRD2-009(ENST00000634537.1)", "TXNRD2-022 ENST00000634471.1", "AGAP1-011(ENST00000635100.1)", "RIPPLY2-003(ENST00000635617.1)"]
+		}
+	},
+
+	"82": {
+		"date": {
+			"month": "9",
+			"year": "2015"
+		}
+	},
+
+	"81": {
+		"date": {
+			"month": "7",
+			"year": "2015"
+		}
+	},
+
+	"80": {
+		"date": {
+			"month": "5",
+			"year": "2015"
+		}
+	},
+
+	"79": {
+		"date": {
+			"month": "3",
+			"year": "2015"
+		}
+	},
+
+	"78": {
+		"date": {
+			"month": "12",
+			"year": "2014"
+		}
+	},
+
+	"77": {
+		"date": {
+			"month": "10",
+			"year": "2014"
 		}
 	},
 
@@ -38,9 +106,11 @@
 			"year": "2014"
 		},
 		"changes": {
-			"Sequence changed": ["ENST00342", "ENST000231", "ENST1111"],
-			"New transcript": ["PCDHA11-002(ENST00000616325.1)", "MED25-201(ENST00000612791.4)","MED25-203(ENST00000617849.4),PXN-AS1-006(ENST00000620336.1)"],
-			"Assembly change": "GRCH38"
+			"Transcript sequence changed": ["RP11-361F15.5-001(ENST00000616480.1)", "CCDC88B-201(ENST00000301897.5)", "PCDHA3-002(ENST00000532566.3)", "PXN-AS1-006(ENST00000620336.1)", "PCDHA1-002(ENST00000504120.3)", "PCDHA13-001(ENST00000617769.1)"],
+			"Protein sequence changed": ["ENSP00000433378.1", "ENSP00000443378.3", "ENSP00000432378.4"],
+			"New transcript": ["PCDHA11-002(ENST00000616325.1)", "MED25-201(ENST00000612791.4)", "MED25-203(ENST00000617849.4)", "PXN-AS1-006(ENST00000620336.1)", "MED25-204(ENST00000618715.4)", "PCDHA11-003(ENST00000617408.1)"],
+			"Assembly changed": "GRCH38",
+			"Retired transcript": ["AGAP1-011(ENST00000635100.1)", "RIPPLY2-003(ENST00000635617.1)"]
 		}
 	},
 
@@ -50,7 +120,46 @@
 			"year": "2014"
 		},
 		"changes": {
-			"Retired transcript": ["ENST0032", "ENT00132"]
+			"Retired transcript": ["GNAS-201(ENST00000306090.11)", "PCDHA8-002(ENST00000378123.4)"]
+		}
+	},
+
+	"74": {
+		"date": {
+			"month": "9",
+			"year": "2013"
+		}
+	},
+
+	"73": {
+		"date": {
+			"month": "6",
+			"year": "2013"
+		}
+	},
+
+	"72": {
+		"date": {
+			"month": "3",
+			"year": "2013"
+		}
+	},
+
+	"71": {
+		"date": {
+			"month": "11",
+			"year": "2012"
+		}
+	},
+
+	"70": {
+		"date": {
+			"month": "8",
+			"year": "2012"
+		},
+		"changes": {
+			"New transcript": ["HLA-B-201(ENST00000639564.1)", "GNAS-072(ENST00000639562.1)"]
 		}
 	}
+
 }
